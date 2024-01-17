d059d38003b6ca8483fa335bea2f0c8ae2758a26
Gabriel <owelcontacto@gmail.com>
2024-01-17 12:55:50 +0100

    Nuevo metodo avanzarMinuto

--- PantallaHoraYFecha.java
+++ PantallaHoraYFecha.java
@@ -6,16 +6,15 @@ public class PantallaHoraYFecha
     private PantallaDosDigitos hora;
     private PantallaDosDigitos minutos;
 
-
     public PantallaHoraYFecha()
     {
         int valorInicial = 1;
         int limitePantallaDia = 32;
         int limitePantallaMes = 13;
-        int limitePantallaAno = 2030;
+        int limitePantallaAno = 100;
         int limitePantallaHora = 24;
         int limitePantallaMinutos = 60;
-        
+
         dia = new PantallaDosDigitos(valorInicial, limitePantallaDia);
         mes = new PantallaDosDigitos(valorInicial, limitePantallaMes);
         ano = new PantallaDosDigitos(valorInicial, limitePantallaAno);
@@ -25,6 +24,36 @@ public class PantallaHoraYFecha
 
     public String getFechaYHora()
     {
-         return hora.getTextoDeLaPantalla() + minutos.getTextoDeLaPantalla() +  "  " + dia.getTextoDeLaPantalla() + "-" + mes.getTextoDeLaPantalla() + "-" + ano.getTextoDeLaPantalla(); 
+        return hora.getTextoDeLaPantalla() + minutos.getTextoDeLaPantalla() +  "  " + dia.getTextoDeLaPantalla() + "-" + mes.getTextoDeLaPantalla() + "-" + ano.getTextoDeLaPantalla(); 
+    }
+
+    public void avanzarMinuto(){ 
+        minutos.incrementaValorAlmacenado();
+
+        if (minutos.getValorAlmacenado() == 0){
+            hora.incrementaValorAlmacenado();
+        }
+        minutos.incrementaValorAlmacenado();
+
+        if (minutos.getValorAlmacenado() == 0) {
+
+            hora.incrementaValorAlmacenado();
+        }
+        if (hora.getValorAlmacenado() == 0) {
+            dia.incrementaValorAlmacenado();
+        }
+        if (dia.getValorAlmacenado() > 30) {
+            dia.setValorAlmacenado(1);
+            mes.incrementaValorAlmacenado();
+        }
+        if (mes.getValorAlmacenado() > 12) {
+            mes.setValorAlmacenado(1);
+            ano.incrementaValorAlmacenado();
+        }
+        if (ano.getValorAlmacenado() > 99) {
+            ano.setValorAlmacenado(1);
+        }
+
     }
 }
+
